179a496eba8d88c9381e471aeed2118ce999a71c
Taras Hlukhovetskyi <taras.hlukhovetskyi@globallogic.com>
2026-03-16 14:59:30 +0200

    [FIX] [UI] Serving pipeline overview - root function link leads back to project page

--- src/components/MonitoringApplicationsPage/MonitoringApplications/monitoringApplications.util.js
+++ src/components/MonitoringApplicationsPage/MonitoringApplications/monitoringApplications.util.js
@@ -52,7 +52,7 @@ export const generateOperatingFunctionsTable = (functions, projectName) => {
     return {
       name: {
         value: func.name,
-        href: generateNuclioLink(`/projects/${projectName}/functions/${nuclioFunctionName}`),
+        href: generateNuclioLink(`/projects/${projectName}/real-time-functions/${nuclioFunctionName}`),
         className: 'table-cell_big'
       },
       status: {
