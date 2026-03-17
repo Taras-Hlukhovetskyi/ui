b952913fdc481bc02ae06dad1b91ca3b7628cdf5
Taras Hlukhovetskyi <taras.hlukhovetskyi@globallogic.com>
2026-03-17 12:32:39 +0200

    formatting

--- src/common/DatePicker/DatePickerView.jsx
+++ src/common/DatePicker/DatePickerView.jsx
@@ -175,7 +175,10 @@ const DatePickerView = React.forwardRef(
                   name={option.id}
                   key={option.id}
                   onClick={() => {
-                    if (option.id !== selectedOption?.id || option.id === CUSTOM_RANGE_DATE_OPTION) {
+                    if (
+                      option.id !== selectedOption?.id ||
+                      option.id === CUSTOM_RANGE_DATE_OPTION
+                    ) {
                       onSelectOption(option)
                     }
                   }}
